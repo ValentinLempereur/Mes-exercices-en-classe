7711410ee5dd7954637fccdb27ee8804153564a1
ValentinLempereur <91312931+ValentinLempereur@users.noreply.github.com>
2021-10-07 15:48:06 +0200

    trouver le catégorie d'âge

---
+++ Execice 1_catégorie d'age/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Execice_1_catégorie_d_age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 0;
+            string categorie = "";
+            Console.WriteLine("Donne moi ton age");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            trouverLaCategori(age, out categorie);
+            Console.WriteLine(categorie);
+        }
+
+        static void trouverLaCategori(int age, out string categorie) {
+            if (age <= 5){
+                categorie = "age trop petit";
+
+            }
+            else if (age <= 7){
+                categorie = "Poussin";
+
+            }
+            else if (age <= 9){
+                categorie = "pupille";
+
+            }
+            else if (age <= 11){
+                categorie = "minime";
+
+            }
+            else if (age <= 14){
+                categorie = "cadet";
+
+            }
+            else{
+                categorie = "age trop grand";
+            }
+        }
+    }
+}
