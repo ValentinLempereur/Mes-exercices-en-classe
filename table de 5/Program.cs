812fb1bb84318eb88b944ef5a1fec0408bd194b5
ValentinLempereur <91312931+ValentinLempereur@users.noreply.github.com>
2021-10-07 16:12:57 +0200

    algo table de 5

---
+++ table de 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace table_de_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "0";
+            Console.WriteLine(nombre);
+        }
+        static void calculer(out string nombre){
+
+
+            for (int n = 1; n <= 100; n++)
+            {
+                int total = n * 5;
+                nombre = nombre + "," + total;
+            }
+
+        }
+
+
+    }
+}
